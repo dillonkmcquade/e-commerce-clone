8b1dec14011249803d107aa03fa5c0a68a26bfe1
Dillon McQuade <dillonkmcquade@gmail.com>
2019-12-18 17:05:18 -0500

    navbar update

--- client/src/components/navigation/navigation.component.jsx
+++ client/src/components/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import "./navigation.styles.scss";
-import logo from "../../logos/logo_transparent.png";
+import logo from "../../logo_transparent.png";
 
 const Navigation = () => {
   return (
@@ -14,31 +14,31 @@ const Navigation = () => {
       >
         <div className="bg-black-80">
           <nav className="dt mw8 w-100 center">
-            <div className="logo link dim pointer">
-              <Link to="/">
-                <img src={logo} alt="logo" />
-              </Link>
-            </div>
-            <div className="dtc v-mid tr pa3">
+                <div className="logo link dim pointer">
+                    <Link to="/">
+                        <img src={logo} alt="logo" />
+                    </Link>
+                </div>
+            <div className=" dtc v-mid tr pa3">
               <input
                 type="search"
                 placeholder="Search"
                 className="input-reset ba b--black-20 pa2 mb2"
               />
               <Link
-                className="f4 fw4 hover-white no-underline white dn dib-ns pv2 ph3"
+                className="f4 fw4 hover-gray no-underline white dn dib-ns pv2 ph3"
                 to="/shop"
               >
                 Shop
               </Link>
               <Link
-                className="f4 fw4 hover-white no-underline white dn dib-l pv2 ph3"
+                className="f4 fw4 hover-gray no-underline white dn dib-l pv2 ph3"
                 to="/contact"
               >
                 Contact
               </Link>
               <Link
-                className="f4 fw4 hover-white no-underline white dib ml2 pv2 ph3 ba"
+                className="f4 fw4 hover-gray no-underline white dib ml2 pv2 ph3 ba"
                 to="/signin"
               >
                 Sign In
