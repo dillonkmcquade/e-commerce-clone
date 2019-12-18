91339d2bc9b2f560c43fb6314d03373664cc9c09
Dillon McQuade <dillonkmcquade@gmail.com>
2019-12-18 12:29:05 -0500

    added collection preview and collection item components and improved file structure

--- client/src/components/directory/directory.data.js
+++ client/src/components/directory/directory.data.js
@@ -1,38 +1,38 @@
 const sections = [
   {
-    title: "HATS",
-    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    title: "ACCESSORIES",
+    imageUrl: "https://media.mnn.com/assets/images/2016/10/camping-lead.jpg.653x0_q80_crop-smart.jpg",
     size: "small",
     id: 1,
-    linkUrl: "hats"
+    linkUrl: "accessories"
   },
   {
-    title: "JACKETS",
-    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    title: "CLOTHING",
+    imageUrl: "https://outdoorgearlab-mvnab3pwrvp3t0.stackpathdns.com/photos/17/40/295552_12775_L2.jpg",
     size: "small",
     id: 2,
-    linkUrl: "jackets"
+    linkUrl: "clothing"
   },
   {
-    title: "SNEAKERS",
-    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    title: "FOOTWEAR",
+    imageUrl: "http://comfortworkboots.com/wp-content/uploads/2017/06/2-Pairs-of-Hiking-Boots.jpg",
     size: "small",
     id: 3,
-    linkUrl: "sneakers"
+    linkUrl: "footwear"
   },
   {
-    title: "WOMENS",
-    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    title: "SHELTERS",
+    imageUrl: "https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg",
     size: "large",
     id: 4,
-    linkUrl: "womens"
+    linkUrl: "shelters"
   },
   {
-    title: "MENS",
-    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    title: "BACKPACKS",
+    imageUrl: "https://www.rei.com/adventures/assets/adventures/images/trip/core/northamerica/gsb_hero",
     size: "large",
     id: 5,
-    linkUrl: "mens"
+    linkUrl: "backpacks"
   }
 ];
 
