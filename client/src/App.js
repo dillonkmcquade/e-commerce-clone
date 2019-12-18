494aaf96e23894b03332cccfecbe00f25decf506
Dillon McQuade <dillonkmcquade@gmail.com>
2019-12-18 15:51:42 -0500

    added navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./App.css";
+import "./App.styles.scss";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
-import ShopPage from './pages/shop/shop.component';
-
+import ShopPage from "./pages/shop/shop.component";
+import Navigation from "./components/navigation/navigation.component";
 
 function App() {
   return (
-    <div>
+    <div className="app">
+      <Navigation className="nav" path={'/'} />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ client/src/components/navigation/navigation.component.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navigation.styles.scss";
+import logo from "../../logos/logo_transparent.png";
+
+const Navigation = () => {
+  return (
+    <header>
+      <div
+        className="cover bg-black bg-left bg-center-l"
+        style={{
+          height: "100%"
+        }}
+      >
+        <div className="bg-black-80">
+          <nav className="dt mw8 w-100 center">
+            <div className="logo link dim pointer">
+              <Link to="/">
+                <img src={logo} alt="logo" />
+              </Link>
+            </div>
+            <div className="dtc v-mid tr pa3">
+              <input
+                type="search"
+                placeholder="Search"
+                className="input-reset ba b--black-20 pa2 mb2"
+              />
+              <Link
+                className="f4 fw4 hover-white no-underline white dn dib-ns pv2 ph3"
+                to="/shop"
+              >
+                Shop
+              </Link>
+              <Link
+                className="f4 fw4 hover-white no-underline white dn dib-l pv2 ph3"
+                to="/contact"
+              >
+                Contact
+              </Link>
+              <Link
+                className="f4 fw4 hover-white no-underline white dib ml2 pv2 ph3 ba"
+                to="/signin"
+              >
+                Sign In
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
