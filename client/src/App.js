20b762d3d421c3e3cc110489c36000fca3bdbd45
dillonkmcquade <dillonkmcquade@gmail.com>
2019-12-20 15:46:27 -0500

    added button component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,12 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Navigation from "./components/navigation/navigation.component";
-import SignInSignUp from './pages/signin-signup/signin-signup.componenet';
-
+import SignInSignUp from "./pages/signin-signup/signin-signup.componenet";
 
 function App() {
   return (
     <div className="app">
-      <Navigation className="nav" path={'/'} />
+      <Navigation className="nav" path={"/"} />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
