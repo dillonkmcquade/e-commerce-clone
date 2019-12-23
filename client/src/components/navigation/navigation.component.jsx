b0f72338561ff241b0636d3cb81baa0e8709f008
dillonkmcquade <dillonkmcquade@gmail.com>
2019-12-23 12:02:49 -0500

    update navigation

--- client/src/components/navigation/navigation.component.jsx
+++ client/src/components/navigation/navigation.component.jsx
@@ -8,41 +8,43 @@ const Navigation = ({ currentUser }) => {
   return (
     <header>
       <div
-        className="cover bg-black bg-left bg-center-l"
+        className="cover bg-left bg-center-l"
         style={{
           height: "100%"
         }}
       >
-        <div className="bg-black-80">
+        <div>
           <nav className="dt mw8 w-100 center">
-            <div className="logo link dim pointer">
-              <Link to="/">
+            <div className="logo pointer">
+              <Link className="dim" to="/">
                 <img src={logo} alt="logo" />
               </Link>
+              <span className="titleLogo">GREEN MOUNTAIN GEAR</span>
             </div>
+
             <div className=" dtc v-mid tr pa3">
               <Link
-                className="f4 fw4 hover-gray no-underline white dn dib-ns pv2 ph3"
+                className="f5 fw4 hover-gray no-underline black dn dib-ns pv2 ph3"
                 to="/shop"
               >
                 Shop
               </Link>
               <Link
-                className="f4 fw4 hover-gray no-underline white dn dib-l pv2 ph3"
+                className="f5 fw4 hover-gray no-underline black dn dib-l pv2 ph3"
                 to="/contact"
               >
                 Contact
               </Link>
               {currentUser ? (
                 <Link
-                  className="f4 fw4 hover-gray no-underline white dib ml2 pv2 ph3 ba"
+                  className="f5 fw4 hover-gray no-underline black dib ml2 pv2 ph3 ba"
                   onClick={() => auth.signOut()}
                 >
                   Sign Out
                 </Link>
               ) : (
                 <Link
-                  className="f4 fw4 hover-gray no-underline white dib ml2 pv2 ph3 ba"
+                  className="f5 fw4 hover-gray no-underline black dib ml2 pv2 ph3 ba"
                   to="/signin"
                 >
                   Sign In
