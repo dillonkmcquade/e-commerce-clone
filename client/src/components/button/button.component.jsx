cf7a08fd2d77fd94e62d9509c5519151628bd540
dillonkmcquade <dillonkmcquade@gmail.com>
2019-12-29 22:19:51 -0500

    added 'add to cart' button on collection item component

--- client/src/components/button/button.component.jsx
+++ client/src/components/button/button.component.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./button.styles.scss";
 
-const Button = ({ children, isGoogleSignIn, bannerlogo, ...otherProps }) => {
+const Button = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  bannerlogo,
+  ...otherProps
+}) => {
   return (
     <button
       className={`${
-        isGoogleSignIn ? "google-sign-in" : bannerlogo ? "bannerlogo" : null
+        isGoogleSignIn
+          ? "google-sign-in"
+          : bannerlogo
+          ? "bannerlogo"
+          : inverted
+          ? "inverted"
+          : null
       } custom-button`}
       {...otherProps}
     >
--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import "./collection-item.styles.scss";
+import Button from "../button/button.component";
 
-
-const CollectionItem = ({id, name, price, imageUrl}) => {
-    return (
-        <div className='collection-item'>
-            <div
-            className="image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-            />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>${price}</span>
-            </div>
-            
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">${price}</span>
       </div>
-    );
-}
-
+      <Button inverted='true'>Add to cart</Button>
+    </div>
+  );
+};
 
 export default CollectionItem;
