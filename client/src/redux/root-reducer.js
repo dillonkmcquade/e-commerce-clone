62bc48efeca9fc3cac3c21723897db229ace03a9
dillonkmcquade <dillonkmcquade@gmail.com>
2019-12-31 14:49:50 -0500

    added redux persist for local storage

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import userReducer from "./user/user.reducer";
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import CartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: CartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
